6f81775dfe5d514f104960f5685ff1b14661fa03
Murilo Ghignatti <muriloghignatti@gmail.com>
2024-05-22 18:27:14 -0300

    Started adding validation layers

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <cstring>
 #include <vector>
+#include <vulkan/vulkan_core.h>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -13,6 +14,15 @@
 
 const uint32_t WIDHT = 800;
 const uint32_t HEIGHT = 600;
+
+const std::vector<const char *> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
 
 class HelloTriangleApplication {
 public:
@@ -34,6 +44,10 @@ private:
   }
 
   void createInstance() {
+    if (enableValidationLayers && !checkValidationLayerSupport()) {
+      throw std::runtime_error(
+          "validation layers requested, but not available!");
+    }
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "Hello Triangle";
@@ -45,11 +59,24 @@ private:
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
+
+    if (enableValidationLayers) {
+      addValidationLayers(createInfo);
+    } else {
+      createInfo.enabledLayerCount = 0;
+    }
+
     getExtensions(createInfo);
 
     if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
       throw std::runtime_error("Failed to create instance!");
     }
+  }
+
+  void addValidationLayers(VkInstanceCreateInfo &createInfo) {
+    createInfo.enabledLayerCount =
+        static_cast<uint32_t>(validationLayers.size());
+    createInfo.ppEnabledLayerNames = validationLayers.data();
   }
 
   void getExtensions(VkInstanceCreateInfo &createInfo) {
@@ -115,7 +142,56 @@ private:
     }
   }
 
-  void initVulkan() { createInstance(); }
+  bool checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    bool layerFound;
+
+    for (auto &neededLayer : validationLayers) {
+      layerFound = false;
+      for (auto &supportedLayer : availableLayers) {
+        if (strcmp(neededLayer, supportedLayer.layerName) == 0) {
+          layerFound = true;
+          break;
+        }
+      }
+      if (!layerFound) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  void initVulkan() {
+    createInstance();
+    pickPhysicalDevice();
+  }
+
+  void pickPhysicalDevice() {
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+    if (deviceCount == 0) {
+      throw std::runtime_error("failed to find GPUs with Vulkan support!");
+    }
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+    for (const auto &device : devices) {
+      if (isDeviceSuitable(device)) {
+        physicalDevice = device;
+        break;
+      }
+    }
+    if (physicalDevice == VK_NULL_HANDLE) {
+      throw std::runtime_error("failed to find a suitable GPU!");
+    }
+  }
+
+  bool isDeviceSuitable(VkPhysicalDevice device) { return true; }
 
   void mainLoop() {
     while (!glfwWindowShouldClose(window)) {
@@ -133,6 +209,7 @@ private:
 private:
   GLFWwindow *window;
   VkInstance instance;
+  VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 };
 
 int main() {
